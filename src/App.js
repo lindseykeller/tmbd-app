56820e0ba9ae60fce0208975473dadd077e968f8
Lindsey Keller <lindseykeller@Lindseys-MacBook-Pro.local>
2023-11-29 22:30:00 -0800

    commiting changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Movie from './components/Movie';
 
 
+
+
 function App() {
   return (
     <div className="App">
-  
       <Movie />
     </div>
   )
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,32 +2,48 @@ import React, { useState, useEffect } from "react";
 
 
 function Movie() {
+    const [movieList, setMovieList] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
-   
-    const [movieList, setMovieList] = useState([])
-    const getMovie  =() => {  
-         const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDk0MDk3MWZlYzJlNWZiNDlkM2QyZTQ3OTdkOWRhNSIsInN1YiI6IjY1NjU2MDE0MTU2Y2M3MDEwY2IzMTUxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S8rXI1em-12sf3qRFhWknWLPRSVxOJnGnyFjK6XsdLI'
-        }
-      };
-      //?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
-      fetch('https://api.themoviedb.org/3/discover/movie', options)
+    //for search tbb later
+    const API_KEY = "10940971fec2e5fb49d3d2e4797d9da5";
+    const discoverEndpoint = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+    const searchEndpoint = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchQuery}`
+    const endpoints = searchQuery ? searchEndpoint : discoverEndpoint;
+
+    const getMovie = () => {  
+        const options = {
+       method: 'GET',
+       headers: {
+         accept: 'application/json',
+         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDk0MDk3MWZlYzJlNWZiNDlkM2QyZTQ3OTdkOWRhNSIsInN1YiI6IjY1NjU2MDE0MTU2Y2M3MDEwY2IzMTUxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S8rXI1em-12sf3qRFhWknWLPRSVxOJnGnyFjK6XsdLI'
+       }
+     };
+     //?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
+    
+         fetch(endpoints, options)
         .then(response => response.json())
         .then(response => setMovieList(response.results))
         .catch(err => console.error(err))
+     
     }
- 
-    
+
         useEffect(() => {
-            getMovie();
-        }, [])
-    
-        console.log(movieList)
+           getMovie();
+       }, [])
+   
+       console.log(movieList);
         return (
             <div>
+                 <div className="searchBar">
+                  <input type="text" value={searchQuery}
+                   onChange = {(e) => {
+                    setSearchQuery(e.target.value)
+                    console.log(e.target.value)
+                   }}
+                  />
+                  <button>Show All</button>
+                </div>
                 {
                     movieList.map((movie) => (
                         <>
